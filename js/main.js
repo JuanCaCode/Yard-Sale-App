24302e902c1976c9aed12b272b7ddbf55d64e477
Juan Camilo Campo <juanccampo95@gmail.com>
2022-12-06 00:26:17 -0500

    En este commit se fusiona el menú de mobile y sus @media queries

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@
 let navEmailLink = document.querySelector("#emailLink")
 let navEmailMenu = document.querySelector(".desktop-menu")
 
+let navMobileToggle = document.querySelector(".menu-mobile-img")
+let navMobileMenu = document.querySelector(".mobile-menu")
+
 navEmailLink.addEventListener('click',toggleNavbarEmailMenu)
+navMobileToggle.addEventListener('click',toggleNavMobileMenu)
 
 function toggleNavbarEmailMenu(){
     navEmailMenu.classList.toggle('active')
+}
+function toggleNavMobileMenu(){
+    navMobileMenu.classList.toggle('active')
+    
 }
