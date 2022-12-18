9a4c0698aac071ae514ba64223f4d4c55153ab58
Juan Camilo Campo <juanccampo95@gmail.com>
2022-12-18 01:43:01 -0500

    actualización de estilos css, sass, creación de carpeta styles y global.scss, importación en App.jsx, configuraciones en webpack.config.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import '../styles/global.scss';
 
 const App = ()=>{
     return (
-        <h1>Hello World</h1>
+        <h1>Hello World, Bienvenidos al curso practico de react</h1>
     );
 }
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path'); //identificará en qué dirección está nuestro proyecto
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //importamos el plugin html que instalamos
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//añadiremos cada configuración necesaria para el proyecto
+module.exports = {
+    entry:'./src/index.js', // donde está el punto de entrada de la app
+    output:{
+        path: path.resolve(__dirname,'dist'), //aquí se crea una carpeta distribution para el código final
+        filename: 'bundle.js' //nombre de bundle o de empaquetado
+    },
+    resolve:{
+        extensions:['.js','.jsx'] //reconoce las extensiones de nuestro proyecto
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/, //se entenderán todos los archivos que sean js o jsx 
+                exclude: /node_modules/, //no quiero que lea en el proyecto
+                use:{
+                    loader:'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/, //lee todos los archivos html
+                use:[
+                    {loader:'html-loader'}
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }), //instancia del plugin
+        new MiniCssExtractPlugin({
+            filename:'[name].css',
+        }),
+    ],
+    devServer:{
+        static: path.join(__dirname,'dist'),
+        port:3000,
+    }
+}
