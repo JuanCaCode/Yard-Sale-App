b4e0487f7bee5fcc490be7eacf2b2e77e1c2a150
Juan Camilo Campo <juanccampo95@gmail.com>
2022-12-18 20:28:30 -0500

    actualización de archivos nuevos, como contenedores y estilos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Layout from "../containers/Layout"
+import Login from "../containers/Login"
+
 import '../styles/global.scss';
 
 const App = ()=>{
     return (
-        <h1>Hello World, Bienvenidos al curso practico de react</h1>
+        <Layout>
+            <Login />
+        </Layout>
     );
 }
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,18 @@ module.exports = {
                     "css-loader",
                     "sass-loader"
                 ]
-            }
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[hash]-[name].[ext]',
+                            },
+                    },
+                    ],
+            },
         ]
     },
     plugins:[
