6da428df365f27e8b773c2215097f2e9766638b4
Juan Camilo Campo <juanccampo95@gmail.com>
2022-12-18 23:42:17 -0500

    actualización de página de notfound con imagen y redirección al home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import NotFoundErrorImg from '../../public/assets/images/404-error.svg';
+
 
 const NotFound = () => {
+    let styleDiv ={
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent:"center",
+        flexDirection:"column",
+        alignItems: "center",
+    }
     return (
-        <div>
-            <h1> No encontrado, error 404.</h1>
+        <div style={styleDiv}>
+            <img  style={{width:"30%"}} src={NotFoundErrorImg} alt="Error 404, no se encontró la página que buscas" />
+            <h3>No hemos encontrado a dónde quieres ir, pero puedes regresar al <a href="/"> inicio.</a></h3>
         </div>
     )
 }
