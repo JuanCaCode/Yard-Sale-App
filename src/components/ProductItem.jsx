995c6347e1a48b5b08cb0843319a6d593b0fcf15
Juan Camilo Campo <juanccampo95@gmail.com>
2022-12-24 10:48:57 -0500

    se actualiza la lista de productos, agregando un seudo elemento::after, y se aplica animación

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,18 +1,19 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import AppContext from '@context/AppContext';
 import iconcart from '@icons/bt_add_to_cart.svg'
+import '@styles/components/ProductItem.scss'
 
 const ProductItem = ({product}) => {
-
     const { addToCart } = useContext(AppContext);
-    
     const handleClick = producto =>{
         addToCart(producto)
     }
     // console.log(product.product.id)
     return (
         <article className="product-card">
-            <img src={product.images[0]} alt={product.title} />
+            <figure className='product-card-img'>
+                <img src={product.images[0]} alt={product.title} />
+            </figure>
             <div className="product-card-info">
                 <div className="details-container">
                     <p>${product.price}</p>
