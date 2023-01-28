3cc5677cde45feb6466bb8b1b238341fd1d94dc5
JuanCaCode <juanccampo95@gmail.com>
2023-01-28 03:40:42 -0500

    actualizacion app

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,28 +1,33 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import AppContext from '@context/AppContext';
 import iconcart from '@icons/bt_add_to_cart.svg'
 import Image from 'next/image';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import styles from '@styles/components/ProductItem.module.scss'
 
-const ProductItem = ({handleDetailToggle,product}) => {
+const ProductItem = ({ handleDetailToggle, product }) => {
     const { addToCart, addToDetail } = useContext(AppContext);
-    const handleClick = producto =>{
+    const handleClick = producto => {
         addToCart(producto);
     }
-    const handleProductDetail = (producto) =>{
+    const handleProductDetail = (producto) => {
         addToDetail(producto);
         handleDetailToggle('open');
     }
-    return ( 
+    return (
         <article className={styles["product-card"]}>
-            <figure className={styles['product-card-img']} onClick={()=>handleProductDetail(product)}>
-                <Image 
-                className={styles["card-img"]} 
-                src={product.images[0]}
-                alt={product.title}
-                fill
-                sizes='(max-width: 768px) 140px,240px'
-                priority={true}
+            <figure className={styles['product-card-img']} onClick={() => handleProductDetail(product)}>
+                <LazyLoadImage
+                    className={styles["card-img"]}
+                    key={product.id}
+                    src={product.images[0]}
+                    alt={product.title}
+                    width={"100%"}
+                    priority={true}
+                    sizes='(max-width: 768px) 140px,340px'
+                    // placeholderSrc={product.images[0]}
+                    effect="blur"
                 />
             </figure>
             <div className={styles["product-card-info"]}>
@@ -30,7 +35,7 @@ const ProductItem = ({handleDetailToggle,product}) => {
                     <p>${product.price}</p>
                     <p>{product.title}</p>
                 </div>
-                <figure onClick={()=>handleClick(product)}>
+                <figure onClick={() => handleClick(product)}>
                     <Image src={iconcart} alt="botón de añadir a carrito de compras" />
                 </figure>
             </div>
